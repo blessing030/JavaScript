5756bb0694971ac8a28614ad5f123dec1af4d5c4
njery <joanwambo97@gmail.com>
2025-03-17 20:52:58 +0100

    this is my first js trial

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log( 'Hello');
+console.log( 'I like pizza');
+console.log('My name is Njery');
+ //window.alert();
+
+ document.getElementById("body").textContent= "Hello World";
+ document.getElementById("myp").textContent = "Hello Mimi"
+
+ let firstName ="Val";
+ let age = "36";
+ let employed ="true";
+
+ document.getElementById("p1").textContent = "your name is Val";
+ document.getElementById("p2").textContent = "your age is 36";
+ document.getElementById("p3").textContent ="you are employed";
